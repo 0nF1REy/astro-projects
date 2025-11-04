798c41803a109f725ba6067a83545a32204050e9
Alan Ryan da Silva Domingues <alanryan619@gmail.com>
2025-11-03 22:04:55 -0300

    main: feat (layout) - Refactored layout components to use BaseLayout and updated middleware for public route handling

--- sleepy-spectrum/src/middleware.ts
+++ sleepy-spectrum/src/middleware.ts
@@ -1,22 +1,20 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/astro/server";
 
-const isPublicRoute = createRouteMatcher(["/", "/login", "/new-account"]);
+const isPublicRoute = createRouteMatcher(["/login", "/new-account"]);
 
 export const onRequest = clerkMiddleware((auth, context) => {
-  const { isAuthenticated, userId } = auth();
+  const { isAuthenticated } = auth();
+  const url = new URL(context.request.url);
+  const path = url.pathname;
 
   if (!isPublicRoute(context.request) && !isAuthenticated) {
     return context.redirect("/login");
   }
 
-  if (isAuthenticated) {
+  if (
+    isAuthenticated &&
+    (path === "/login" || path === "/new-account" || path === "/")
+  ) {
     return context.redirect("/conversions");
-  }
-
-  const url = new URL(context.request.url);
-  const path = url.pathname;
-
-  if (isAuthenticated && path === "/") {
-    return context.redirect("/login");
   }
 });
